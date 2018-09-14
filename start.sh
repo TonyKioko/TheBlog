b97e7cc2023fd62ca35f2f3e30cea681ebf2fb20
TonyKioko <tonykioko384@gmail.com>
2018-09-14 12:11:53 +0300

    add registration form

---
+++ start.sh
@@ -0,0 +1,6 @@
+export SECRET_KEY=12345678
+# export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://tony:1234@localhost/minpitch'
+export MAIL_USERNAME=tonykm101@gmail.com
+export MAIL_PASSWORD=acc206wk5
+
+python3.6 manage.py server
