cd3c7012eed6aa9453e580b63c799339f65511a9
TonyKioko <tonykioko384@gmail.com>
2018-09-14 14:27:56 +0300

    profile pic issues

--- app/main/views.py
+++ app/main/views.py
@@ -7,7 +7,7 @@ from .. import photos, db
 from datetime import datetime
 
 @main.route('/')
-@login_required
+# @login_required
 def home():
 
     '''
@@ -21,3 +21,30 @@ def home():
     message= 'Welcome to the Blog'
     # return "Hello, World"
     return render_template('home.html',title=title,message=message)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
