a670c1299eaa0bf2aaf2b096f372505f97cb928e
TonyKioko <tonykioko384@gmail.com>
2018-09-16 20:40:26 +0300

    add tests for models

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,5 +1,5 @@
 import unittest
-from app.models import User,Posts
+from app.models import User,Posts,Comments
 
 class UserModelTest(unittest.TestCase):
 
@@ -15,3 +15,53 @@ class UserModelTest(unittest.TestCase):
 
     def test_password_verification(self):
         self.assertTrue(self.new_user.verify_password('banana'))
+
+class PostsModelTest(unittest.TestCase):
+
+    def setUp(self):
+
+        self.new_post = Posts(title='Terabyte', body='This is a new post',category='Technology')
+
+    def test_instance(self):
+        '''
+        Test case to check if new_post is an instance of Posts class
+        '''
+        self.assertTrue( isinstance( self.new_post, Posts) )
+
+    def test_save_post(self):
+        '''
+        Test case to check if a post is saved to the database
+        '''
+        self.new_post.save_post()
+
+        self.assertTrue( len(Posts.query.all()) > 0 )
+
+class TestComments(unittest.TestCase):
+    '''
+    Test class to test behaviours of the Comments class
+    Args:
+        unittest.TestCase : Test case class that helps create test cases
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_comment = Comments(the_comment="This is a test comment")
+
+    def test_instance(self):
+        '''
+        Test to check if new_comment is an instance of Comments
+        '''
+
+        self.assertTrue( isinstance( self.new_comment, Comments) )
+
+    def test_save_comment(self):
+        '''
+        Test case to check if comment is saved to the database
+        '''
+
+        self.new_comment.save_comment()
+
+        self.assertTrue( len(Comments.query.all()) > 0)
+
